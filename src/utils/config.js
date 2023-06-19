aada29304d53d5b5602d75a4a79401235e47c7f0
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-06-19 05:31:19 +0530

    Resolved Mixed Content Error

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ export const YOUTUBE_FETCH_URL =
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_URL =
-  "http://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q=";
 
 export const YOUTUBE_SEARCH_RESULT_URL =
   "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=" +
